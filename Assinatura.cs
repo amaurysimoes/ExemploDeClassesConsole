03c1a0b0711f2bcf1f163b9f9f3cc63c4b1f3a88
Amaury Huerb Simões <31355114+amaurysimoes@users.noreply.github.com>
2021-04-06 11:52:51 -0400

    Add files via upload

---
+++ Assinatura.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassesDevmedia
+{
+    public class Assinatura : Produto, IExpiravel
+    {
+        private DateTime dataExpiracao;
+        public DateTime DataEspiracao { get => dataExpiracao; set => dataExpiracao = value; }
+        
+        public TimeSpan GetTempoRestante()
+        {
+            return dataExpiracao - DateTime.Today;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesDevmedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assinatura a = new Assinatura
+            {
+                Codigo = 123,
+                Nome = "Assinatura MVP",
+                Preco = 69.99M,
+                DataEspiracao = DateTime.Today.AddMonths(12)
+            };
+            Console.WriteLine($"Dias restante de assinatura: {a.GetTempoRestante().Days}");
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
